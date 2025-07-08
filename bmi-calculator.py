30de68791fa347da208d9af40ea8cea46dac1906
Anuj Savant <anujsavant340@gmail.com>
2025-07-08 07:58:40 +0530

    New Update

---
+++ bmi-calculator.py
@@ -0,0 +1,10 @@
+# Python Project to implement BMI Calculator
+# Author:Anuj Savant
+# Date: 08/07/25
+
+height = float(input("Enter the height in metres:"))
+weight = float(input("Enter the weight in kilograms:"))
+
+bmi = weight/(height * height)
+
+print("Your BMI(Body Mass Index) is :", bmi)calculator.py
